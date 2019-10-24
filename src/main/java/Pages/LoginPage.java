591c307be84945bdc9b5e20ea4dbb16e324cf2d3
simon <simon.novshadyan@magentrix.com>
2019-10-24 17:14:13 -0400

    first commit

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,123 @@
+package Pages;
+
+import Base.TestBase;
+import Utilities.Utils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class LoginPage extends TestBase {
+
+@FindBy(xpath = "//img[@src='/Contents/Assets/themes/falcon2018/img/logo-blue.png']")
+    WebElement Logo;
+
+@FindBy(xpath="//a[@class='signup-link']")
+    WebElement TopSignUp;
+
+@FindBy(xpath="//a[@class='signin-link']")
+    WebElement TopSignIn;
+
+@FindBy(xpath="//p[text()='Welcome to the portal.']")
+    WebElement Welcome;
+
+@FindBy(xpath="//input[@id='UserName']")
+    WebElement UserName;
+
+@FindBy(xpath = "//input[@id='Password']")
+    WebElement Password;
+
+@FindBy(xpath="//input[@value='Login']")
+    WebElement LoginBtn;
+
+@FindBy(xpath = "//input[@name='RememberMe']")
+    WebElement RememberMe;
+
+@FindBy(xpath = "//a[text()='Forgot your password']")
+    WebElement ForgotPWD;
+
+@FindBy(xpath = "//h4[text()='Not a Member Yet?']")
+    WebElement NotMember;
+
+@FindBy(xpath = "//a[text()='Sign Up']")
+    WebElement SignUpBtn;
+
+@FindBy(xpath = "//a[text()='Partner Home']")
+WebElement Home;
+
+
+    public LoginPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public boolean verify_Logo() throws Exception{
+        Utils.HEY(Logo);
+        return Logo.isDisplayed();
+    }
+
+    public boolean verify_TopSignUp() throws Exception{
+        Utils.HEY(TopSignUp);
+        return TopSignUp.isDisplayed();
+    }
+
+    public boolean verify_TopSignIn() throws Exception{
+        Utils.HEY(TopSignIn);
+        return TopSignIn.isDisplayed();
+    }
+
+    public boolean verify_Welcome() throws Exception{
+        Utils.HEY(Welcome);
+        return Welcome.isDisplayed();
+    }
+
+    public boolean verify_UserName() throws Exception{
+        Utils.HEY(UserName);
+        return UserName.isDisplayed();
+    }
+
+    public boolean verify_Password() throws Exception{
+        Utils.HEY(Password);
+        return Password.isDisplayed();
+    }
+
+    public boolean verify_LoginBtn() throws Exception{
+        Utils.HEY(LoginBtn);
+        return LoginBtn.isDisplayed();
+    }
+
+    public boolean verify_RememberMe() throws Exception{
+        Utils.HEY(RememberMe);
+        return RememberMe.isDisplayed();
+    }
+
+    public boolean verify_ForgotPWD() throws Exception{
+        Utils.HEY(ForgotPWD);
+        return ForgotPWD.isDisplayed();
+    }
+
+    public boolean verify_NotMember() throws Exception{
+        Utils.HEY(NotMember);
+        return NotMember.isDisplayed();
+    }
+
+    public boolean verify_SignUpBtn() throws Exception{
+        Utils.HEY(SignUpBtn);
+        return SignUpBtn.isDisplayed();
+    }
+
+
+    public boolean verify_Login() throws Exception{
+
+        UserName.sendKeys(prop.getProperty("Username"));
+        Password.sendKeys(prop.getProperty("Password"));
+        LoginBtn.click();
+//        Utils.WFEV(By.xpath("//a[text()='Partner Home']"));
+        return Home.isDisplayed();
+
+    }
+
+
+
+}
---
+++ src/test/java/TestCases/LoginPageTest.java
@@ -0,0 +1,122 @@
+package TestCases;
+
+import Base.TestBase;
+import Pages.LoginPage;
+import Utilities.Utils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends TestBase {
+
+    LoginPage loginpage;
+    private static Logger log = LogManager.getLogger(LoginPageTest.class);
+
+    public LoginPageTest(){
+        super();
+    }
+
+    @BeforeMethod
+    public void  setup(ITestResult result){
+        loginpage = new LoginPage();
+        log.info("Testing " + result.getMethod().getMethodName());
+    }
+
+
+    @Test(priority = 1)
+    public void TitleTest() {
+
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Guest Home Page | Falcon Tech Community | Trancending Value");
+    }
+
+
+    @Test(priority = 2)
+    public void Test_Logo() throws Exception{
+        Assert.assertTrue(loginpage.verify_Logo());
+    }
+
+    @Test(priority = 3)
+    public void Test_TopSignUp() throws Exception{
+        Assert.assertTrue(loginpage.verify_TopSignUp());
+    }
+
+    @Test(priority = 4)
+    public void Test_TopSignIn() throws Exception{
+        Assert.assertTrue(loginpage.verify_TopSignIn());
+    }
+
+    @Test(priority = 5)
+    public void Test_Welcome() throws Exception{
+        Assert.assertTrue(loginpage.verify_Welcome());
+    }
+
+    @Test(priority = 6)
+    public void Test_UserName() throws Exception{
+        Assert.assertTrue(loginpage.verify_UserName());
+    }
+
+    @Test(priority = 7)
+    public void Test_Password() throws Exception{
+        Assert.assertTrue(loginpage.verify_Password());
+    }
+
+    @Test(priority = 8)
+    public void Test_LoginBtn() throws Exception{
+        Assert.assertTrue(loginpage.verify_LoginBtn());
+    }
+
+    @Test(priority = 9)
+    public void Test_RememberMe() throws Exception{
+        Assert.assertTrue(loginpage.verify_RememberMe());
+    }
+
+    @Test(priority = 10)
+    public void Test_ForgotPWD() throws Exception{
+        Assert.assertTrue(loginpage.verify_ForgotPWD());
+    }
+
+    @Test(priority = 11)
+    public void Test_NotMember() throws Exception{
+        Assert.assertTrue(loginpage.verify_NotMember());
+    }
+
+    @Test(priority = 12)
+    public void Test_SignUpBtn() throws Exception{
+        Assert.assertTrue(loginpage.verify_SignUpBtn());
+    }
+
+    @Test(priority = 13)
+    public void Test_Login() throws Exception{
+        Assert.assertTrue(loginpage.verify_Login());
+    }
+
+
+
+
+
+
+
+
+
+    @AfterMethod
+    public void getResults(ITestResult result) {
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            Utils.captureScreenshot(driver, result.getMethod().getMethodName() );
+            log.info("Test: " + result.getMethod().getMethodName() + " == Failed");
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            log.info("Test: " + result.getMethod().getMethodName() + " == Passed");
+        } else if (result.getStatus() == ITestResult.SKIP){
+            log.info("Test: " + result.getMethod().getMethodName() + " == Skipped");
+
+        }
+    }
+
+
+
+}
